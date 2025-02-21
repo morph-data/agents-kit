ff46b685cd5b2fc8bf43722fdd7658745e991e7b
Keita Mitsuhashi <keita.mitsuhashi.83@gmail.com>
2025-02-21 16:52:24 +0900

    Update meta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { Card } from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] w-full">
-      <main className="flex flex-col gap-4 row-start-2 items-center w-full max-w-[1440px]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen px-8 pb-20 gap-8 sm:px-20 font-[family-name:var(--font-geist-sans)] w-full container-wrapper">
+      <main className="flex flex-col gap-4 row-start-2 items-center w-full max-w-[1440px] container">
         <div className="flex gap-2 items-center">
           <Icon iconNode={tieBow} size={72} />
           <h1 className="font-bold text-5xl">Agents Kit</h1>
